5c8ac31ec4ccbe6360813d53654f02643a33f06e
AustinSFleet <austinsfleet@gmail.com>
2017-09-17 11:25:16 -0500

    added html files, started js work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,71 @@
 var friends = [
-    {
-        
-    }
-]
+    
+      {
+        'name':'Mark',
+        'image': 'https://i.imgur.com/0348EJf.jpg',
+        'scores':[
+            5,
+            4,
+            3,
+            2,
+            1,
+            2,
+            3,
+            4,
+            5,
+            5
+          ]
+      },
+    
+      {
+        'name':'Austin',
+        'image': 'https://i.imgur.com/sue9AXv.jpg',
+        'scores':[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      {
+        'name':'Lisa',
+        'image': 'https://i.imgur.com/XA0jdd1.jpg',
+        'scores':[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+      },
+      {
+        'name':'Tommy',
+        'image': 'https://i.imgur.com/nDzFTd6.jpg',
+        'scores':[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      
+    ];
+
+    module.exports = friends;
